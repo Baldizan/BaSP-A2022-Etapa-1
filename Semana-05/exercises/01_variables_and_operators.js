6c613bdc1582f9c40200db00048da84022559dc5
Mariana Alvarez Baldizan <marezbaldizan@gmail.com>
2022-09-17 10:36:35 -0300

    BaSP-W05-2022: Finish exercise 2

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,22 +1,22 @@
-console.log("-------------------------EXERCISE 1 : VARIABLES AND OPERATORS-------------------------")
+console.log("-------------------------EXERCISE 1 : VARIABLES AND OPERATORS-------------------------");
 
-console.log("------------EXERCISE 1.a------------")
+console.log("------------EXERCISE 1.a------------");
 
 var firstNumber = 13;
 var secondNumber = 48;
 var sumResult = firstNumber + secondNumber;
 
-console.log(firstNumber + "+" + secondNumber + "=" + sumResult)
+console.log(firstNumber + "+" + secondNumber + "=" + sumResult);
 
-console.log("------------EXERCISE 1.b------------")
+console.log("------------EXERCISE 1.b------------");
 
 var myVariable = "Hello";
 var mySecVariable = "World";
 var concatenatedVars = myVariable + " " + mySecVariable;
 
-console.log(concatenatedVars)
+console.log(concatenatedVars);
 
-console.log("------------EXERCISE 1.c------------")
+console.log("------------EXERCISE 1.c------------");
 
 var myVariableLength = "Hello";
 var mySecVariableLength = "World";
@@ -30,4 +30,4 @@ console.log(
     "' have an amount of " +
     allVariablesLength +
     " letters"
-)
+);
--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -2,12 +2,63 @@ console.log("-------------------------EXERCISE 2 : STRINGS----------------------
 
 console.log("------------EXERCISE 2.a------------");
 
+var upperStr = "What does the fox say?"
+
+console.log(upperStr.toUpperCase());
+
 console.log("------------EXERCISE 2.b------------");
+
+var subVariable = "Lorem ipsum dolor sit amet consectetur"
+var subVariableResult = subVariable.substring(0, 5);
+
+console.log(
+    "My variable is '" +
+    subVariable +
+    "' and its first five characters are '" +
+    subVariableResult + "'"
+)
 
 console.log("------------EXERCISE 2.c------------");
 
+var subEndVariable = "Lorem ipsum dolor sit amet consectetur"
+var subEndVariableResult = subVariable.substring(35, 38);
+
+console.log(
+    "My variable is '" +
+    subEndVariable +
+    "' and its last three characters are '" +
+    subEndVariableResult + "'"
+)
+
 console.log("------------EXERCISE 2.d------------");
+
+var capitalizedVariable = "lorem ipsum dolor sit amet consectetur"
+var resultCapitalizedVariable =
+    capitalizedVariable.substring(0, 1).toUpperCase() +
+    capitalizedVariable.substring(1, 38).toLowerCase();
+
+console.log(resultCapitalizedVariable);
 
 console.log("------------EXERCISE 2.e------------");
 
+var indexedVariable = "Lorem ipsum dolor sit amet consectetur"
+var firstSpIndex = indexedVariable.indexOf(" ");
+
+console.log("My string is '" +
+    indexedVariable +
+    "' and the first space is indexed at the "+
+    firstSpIndex +
+    "th column"
+);
+
 console.log("------------EXERCISE 2.f------------");
+
+var longWord = "esternocleidomastoideo"
+var secLongWord = "electroencefalografista"
+var capitalizedLongWords =
+    longWord.substring(0, 1).toUpperCase() +
+    longWord.substring(1, 39).toLowerCase() + " " +
+    secLongWord.substring(0, 1).toUpperCase() +
+    secLongWord.substring(1, 42).toLowerCase();
+
+console.log(capitalizedLongWords);
